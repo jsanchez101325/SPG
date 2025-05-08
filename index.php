87f0f861006bd1cccec841578e906ecc5bfd4edc
jose <jsanchez1virtual@gmail.com>
2025-05-08 14:55:55 -0400

    Cambio de color en el tiyulo

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ $estado_general = $stmt_estado_general->fetchAll(PDO::FETCH_ASSOC);
         <a href="index.php"><i class="fas fa-home"></i> Inicio</a>
         <a href="proyectos.php"><i class="fas fa-clipboard-list"></i> Proyecto</a>
         <a href="presupuesto.php"><i class="fas fa-dollar-sign"></i> Presupuesto</a>
-        <a href="recursos.php"><i class="fas fa-dollar-sign"></i> Recursos</a>
+        <a href="recursos.php"><i class="fa-solid fa-money-bill"></i> Recursos</a>
         <a href="comunicacion.php"><i class="fas fa-comments"></i> Comunicación</a>
         <a href="sobre.html"><i class="fas fa-info-circle"></i> Sobre el Proyecto</a>
     </nav>
